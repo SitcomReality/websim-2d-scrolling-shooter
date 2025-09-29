b60b629adeb182845906821ce96e2eca7b29fc97
SitcomReality <SitcomReality@websim.ai>
2025-09-29 01:21:25 +0000

    v109: The below code was accidentally removed from /game/LevelUpManager.js. Can you please restore it? <deletedCode> export class LevelUpManager {     constructor(gameState, uiManager, upgradeSystem, player, enemySpawner) {         this.gameState = gameSta

--- game/LevelUpManager.js
+++ game/LevelUpManager.js
@@ -1,3 +1,12 @@
+export class LevelUpManager {
+    constructor(gameState, uiManager, upgradeSystem, player, enemySpawner) {
+        this.gameState = gameState;
+        this.uiManager = uiManager;
+        this.upgradeSystem = upgradeSystem;
+        this.player = player;
+        this.enemySpawner = enemySpawner;
+    }
+    
     handleLevelUp() {
         this.gameState.level++;
         this.gameState.xp = 0;
@@ -16,3 +25,29 @@
         this.enemySpawner.unlockEnemyTypes(this.gameState.level);
     }
 
+    continueAfterLevelUp() {
+        this.gameState.isPausedForLevelUp = false;
+        this.gameState.levelUpDelayTimer = 0;
+        this.uiManager.hideLevelUp();
+
+        const upgradeChoices = this.upgradeSystem.generateUpgradeChoices(
+            this.gameState.level,
+            this.upgradeSystem.playerUpgrades
+        );
+
+        this.gameState.isPausedForLevelUp = true;
+        this.uiManager.showUpgradeSelection(upgradeChoices, this.upgradeSystem);
+
+        // Ensure enemy unlocks are applied for the new level before updating panels
+        this.enemySpawner.unlockEnemyTypes(this.gameState.level);
+    }
+
+    handleUpgradeSelection(index) {
+        const choices = this.uiManager.upgradeChoices;
+        if (choices[index]) {
+            this.upgradeSystem.applyUpgrade(choices[index], this.player);
+            this.uiManager.hideUpgradeSelection();
+            this.gameState.isPausedForLevelUp = false;
+        }
+    }
+}
