0325c26de4f84c0836ae24ca0b270958a3374350
SitcomReality <SitcomReality@websim.ai>
2025-09-30 19:31:32 +0000

    v211: error: Uncaught TypeError: this.uiManager.off is not a function TypeError: this.uiManager.off is not a function     at openShopOnce      at /game/utils/EventEmitter.js:15:52     at Array.forEach      at UIManager.emit      at UIManager.handleUpgradeS

--- game/utils/EventEmitter.js
+++ game/utils/EventEmitter.js
@@ -9,6 +9,11 @@ export class EventEmitter {
         }
         this.events[event].push(callback);
     }
+
+    off(event, callback) {
+        if (!this.events[event]) return;
+        this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
     
     emit(event, ...args) {
         if (this.events[event]) {
